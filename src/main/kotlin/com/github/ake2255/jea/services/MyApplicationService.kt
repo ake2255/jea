88be9681ac8b730c4677e4e1fe081170382c570f
GitHub Action <action@github.com>
2022-06-28 16:07:37 +0000

    Template cleanup

--- src/main/kotlin/com/github/ake2255/jea/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/ake2255/jea/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.ake2255.jea.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.ake2255.jea.MyBundle
 
 class MyApplicationService {
 
